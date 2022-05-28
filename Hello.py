dbf38addfc200f1428d9869df1616ee601f340fb
Knpand <f.kntr514@gmail.com>
2022-05-28 21:59:32 +0900

    flask app

--- Hello.py
+++ Hello.py
@@ -13,7 +13,7 @@ def hello():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
 
     # app.run()
