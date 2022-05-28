d8843e217c00d6579a84707997e730dc9afb10f3
Knpand <f.kntr514@gmail.com>
2022-05-28 20:58:10 +0900

    flask app

--- Hello.py
+++ Hello.py
@@ -1,2 +1,20 @@
+# def hello():
+#     print("HelloWorld")
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
 def hello():
-    print("HelloWorld")
+    name = "Hello World"
+    return name
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+## おまじない
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
