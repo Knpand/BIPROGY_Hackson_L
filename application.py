2784b53afe83ae41a39491a9f8457fb9d4e0e766
Takumi <t-nakayama-1w1@eagle.sophia.ac.jp>
2022-05-29 10:41:45 +0900

    add linebot setting

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -12,11 +12,12 @@ from linebot.models import (
 )
 import os
 import datetime 
+# from local_config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
- 
-YOUR_CHANNEL_ACCESS = os.environ.get("access_token")
-YOUR_CHANNEL_SECRET = os.environ.get("channel_secret")
+
+YOUR_CHANNEL_ACCESS = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -40,42 +41,57 @@ def callback():
     return 'OK'
 
 
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-db.init(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://{}:{}@{}/{}?charset=utf8".format("root", "", "localhost", "test")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     line_id = db.Column(db.Text(), nullable=False)
     line_name = db.Column(db.Text(), nullable=False)
+    mail =  db.Column(db.Text(), nullable=True)
     state = db.Column(db.Integer, primary_key=True)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    state_list = ['やばい','普通','良い']
+    state_list = ['やばい...','普通','良い!']
     state = ['1','2','3']
 
-    if event.message.text in state:
-        user_list = [ _user.line_id for _user in User.query.all()]        
-        user_id = event.source.userID 
-        if user_id in user_list:
-            _user = User.query.filter(User.line_id == user_id).first()
-            _user.state = int(event.message.text)
-            db.session.add(_user)
-            db.session.commit()
-        else:
-            _user = User()
-            _user.line_id = user_id
-            _user.line_name = line_bot_api.get_profile(user_id = user_id)
-            db.session.add(_user)
-            db.session.commit()
-    else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="入力してね"))
+    # if event.message.text in state:
+    #     user_list = [ _user.line_id for _user in User.query.all()]        
+    #     user_id = event.source.userID 
+    #     if user_id in user_list:
+    #         _user = User.query.filter(User.line_id == user_id).first()
+    #         _user.state = int(event.message.text)
+    #         db.session.add(_user)
+    #         db.session.commit()
+    #     else:
+    #         _user = User()
+    #         _user.line_id = user_id
+    #         _user.line_name = line_bot_api.get_profile(user_id = user_id)
+    #         _user.state = int(event.message.text)
+    #         db.session.add(_user)
+    #         db.session.commit()
+    # else:
+    #     line_bot_api.reply_message(event.reply_token,TextSendMessage(text="入力してね"))
+
+    items = [QuickReplyButton(action=MessageAction(label=state_list[i], text=state[i])) for i in range(len(state_list))]
+    messages = TextSendMessage(text='現在の進捗はどうですか？',quick_reply=QuickReply(items=items))
+    line_bot_api.reply_message(event.reply_token, messages=messages)
+
+# Botを登録しているユーザに一括送信
+def notify():
+    state_list = ['やばい...','普通','良い!']
+    state = ['1','2','3']
+    items = [QuickReplyButton(action=MessageAction(label=state_list[i], text=state[i])) for i in range(len(state_list))]
+    messages = TextSendMessage(text='現在の進捗はどうですか？',quick_reply=QuickReply(items=items))
+    line_bot_api.broadcast(messages)
+
 if __name__ == "__main__":
     port = os.getenv("PORT")
     app.run(host="0.0.0.0",port=port)
-    
